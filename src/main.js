dada4d558056ba254ae9cb06469bcc81533db89c
your name <453380878@qq.com>
2019-04-20 16:05:14 +0800

    详情页

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { Tabbar, TabbarItem, Stepper, SubmitBar, Checkbox, CheckboxGroup, Card, Swipe, SwipeItem, Search, Tab, Tabs, Badge, BadgeGroup, Button } from 'vant'
+import { Tabbar, TabbarItem, Stepper, SubmitBar, Checkbox, CheckboxGroup, Card, Swipe, SwipeItem, Search, Tab, Tabs, Badge, BadgeGroup, Button, Panel,  GoodsActionBigBtn,
+  GoodsActionMiniBtn,GoodsAction } from 'vant'
 
-Vue.use(Tabbar).use(TabbarItem).use(Stepper).use(SubmitBar).use(Checkbox).use(CheckboxGroup).use(Card).use(Swipe).use(SwipeItem).use(Search).use(Tab).use(Tabs).use(Badge).use(BadgeGroup).use(Button)
+Vue.use(Tabbar).use(TabbarItem).use(Stepper).use(SubmitBar).use(Checkbox).use(CheckboxGroup).use(Card).use(Swipe).use(SwipeItem).use(Search).use(Tab).use(Tabs).use(Badge).use(BadgeGroup).use(Button).use(Panel).use(GoodsActionBigBtn)
+.use(GoodsActionMiniBtn).use(GoodsAction)
 
 Vue.config.productionTip = false
 
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,39 +1,226 @@
 <template>
-  <div>
-    <h1>详情页</h1>
-    <ul>
-      <li>商品ID:{{ productData.id }}</li>
-      <li>商品名称{{ productData.name }}</li>
-      <li>商品价格:{{ productData.price }}</li>
-    </ul>
+  <div class="product-detail">
+    <div class="title-list">
+      <div class="title-detail">商品详情</div>
+      <a href="#">
+        <img
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAYAAAAehFoBAAAAAXNSR0IArs4c6QAAAntJREFUWAntmT9Lw0AYxpvWTtVFUBCc+4dApTiImy1O6jfQTcHN0Q/h6OifTT+CTqL1E3QKLZ0FB8GphUJp6/PUvsc1TVWaXFIhB+3dvbnL+7unb5PLm0QiLrEC/1sBy40/GAysQqFw0u/3jy3LstFfdI9B/6HZbB542BPZbPYB9j2vY7/Z4K8Ff04ymbyt1+s36A/cc5K6AaBr+Xz+CbBXsG9NgdWnBNoe+duif3KQx+1AKYzBFgehrmDQG1Z5jhVWsdJ39yRTfdu2l3u93iGAL+kD/p8bjcaurvSCOGcYCGw6nd5wHOdTjoVVp1KphW63eyr+yEMu9K/FpkKCMUsjlY0CtlgsrnY6nWcg2Pgwjs/II1xssyhgyM6B/BmqrMMspVJpRYfNZDJlqH1PBuESHhUSkH94NQgzZglB2Ha7/YLmUFnC1mq1Dx7DFSchXOyzKIW/u+F+/wQ7jSQy4FlguYhIgGeFjQTYD2zowH5hQwUOAjY04KBgQwEOEtY4cNCwRoFNwBoDzuVyS9Nut3Tqpxi5cWDDsgmoib2BH1CZawQYG6hXbA/LAN+WjYw49Fur3ZrfE+nzAcpnsapum7H9iHljz3VGgGeEm5iGB919t9FISLidBNmPgYNU0+tcscJeqgRpm2uFmfYapb7Umuf6sgbKiRzdXCusZNUaMbAmhpGmUhj3/xY9eKU4jXj+5aTCIVwyXAEjJeTQiHpHDkZZC4dwCYsCxnbwlkZkCy+Yp5UBUdT0Tw76Fi7hmKuENsOAyo5g1xEOEwltBcwVcAIG32FSRVYUVU1YqHvkzqaOARMOsH95KWNkHfyDMWYZBgD1fCljxHF8Uk2BL8xJSqngKBzOAAAAAElFTkSuQmCC"
+        >
+      </a>
+    </div>
+
+    <div class="swipe">
+      <van-swipe :autoplay="3000" @change="onChange">
+        <van-swipe-item>
+          <img
+            src="https://fms-image.missfresh.cn/48d3886a0dda46b29d4da539aafbe286.jpg?iopcmd=thumbnail&type=4&width=640"
+          >
+        </van-swipe-item>
+        <van-swipe-item>
+          <img
+            src="https://fms-image.missfresh.cn/2c9fdd18fdc94610b59d41c937fcb975.jpg?iopcmd=thumbnail&type=4&width=640"
+            alt
+          >
+        </van-swipe-item>
+        <van-swipe-item>
+          <img
+            src="https://fms-image.missfresh.cn/afce534a1e1d40a2bebf5df849d45196.jpg?iopcmd=thumbnail&type=4&width=640"
+            alt
+          >
+        </van-swipe-item>
+        <van-swipe-item>
+          <img
+            src="https://fms-image.missfresh.cn/307d5a290863457d982ae2e9c91fde46.jpg?iopcmd=thumbnail&type=4&width=640"
+            alt
+          >
+        </van-swipe-item>
+        <div class="custom-indicator" slot="indicator">{{ current + 1 }}/4</div>
+      </van-swipe>
+    </div>
+    <dir class="product-describe">
+      <van-panel title="供港品质 甜脆出口11国" desc="供港烟台苹果4个760g起">
+        <div class="product-describe-bottom">
+          <div class="jiage">￥15.9</div>
+          <div class="yishou">已售44536份</div>
+        </div>
+      </van-panel>
+    </dir>
+    <div class="product-attrs">
+      <span>· 产地烟台</span>
+      <span>· 次日达</span>
+      <span>· 实付满69包邮</span>
+    </div>
+    <div class="product-redpack">
+      <div class="main">
+        <img src="https://j-image.missfresh.cn/img_20171029215450695.png">
+        <span class="share-product-text">
+          该商品分享可领49减10红包
+          <i></i>
+        </span>
+        <span class="go-share-btn">去分享></span>
+      </div>
+    </div>
+    <div class="foot">
+     <van-goods-action>
+  <van-goods-action-mini-btn
+    icon="chat-o"
+    text="客服"
+  />
+  <van-goods-action-mini-btn
+    icon="cart-o"
+    text="购物车"
+
+  />
+  <van-goods-action-big-btn
+    text="加入购物车"
+
+  />
+  <van-goods-action-big-btn
+    primary
+    text="立即购买"
+
+  />
+</van-goods-action>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
 export default {
-  name: 'detail',
-  data () {
+  data() {
     return {
-      productData: {}
-    }
+      current: 0
+    };
   },
 
   methods: {
-    getproductData () {
-      axios.get('/static/detail.json').then(res => {
-        let data = res.data
-        var tmp = data.find(item => {
-          return item.id === parseInt(this.$route.params.id)
-        })
-        this.productData = tmp
-      })
+    onChange(index) {
+      this.current = index;
     }
-  },
-
-  created () {
-    this.getproductData()
   }
-
-}
+};
 </script>
+
+
+<style lang="less">
+.product-detail {
+  position: fixed;
+  background-color: #ffffff;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  .title-list {
+    text-align: center;
+    width: 100%;
+    height: 44px;
+    line-height: 44px;
+    font-size: 16px;
+    margin: 0 auto;
+    .title-detail {
+      height: 100%;
+      line-height: 44px;
+      border-bottom: 3px solid transparent;
+      display: inline-block;
+      box-sizing: border-box;
+      border-bottom-color: #ff4891;
+      color: #ff4891;
+    }
+    a {
+      position: absolute;
+      top: 13px;
+      right: 10px;
+      img {
+        display: block;
+        width: 22px;
+        height: 22px;
+      }
+    }
+  }
+}
+
+img {
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+.custom-indicator {
+  position: absolute;
+  top: 220px;
+  right: 20px;
+  font-size: 16px;
+  color: #ccc;
+}
+.product-describe {
+  padding-left: 2px;
+  padding-top: 20px;
+  span {
+    font-size: 18px;
+    color: #474245;
+    font-weight: 700;
+  }
+  .van-cell__label {
+    font-size: 16px;
+    color: #474245;
+  }
+  .product-describe-bottom {
+    padding-top: 15px;
+    .jiage {
+      float: left;
+      padding-left: 20px;
+      color: #ff4891;
+      font-weight: 900;
+    }
+    .yishou {
+      float: right;
+      padding-right: 15px;
+      color: #969696;
+    }
+  }
+}
+.product-attrs {
+  padding-top: 45px;
+  span {
+    padding: 0 10px 0 10px;
+    color: #262626;
+    font-size: 12px;
+  }
+}
+.product-redpack {
+  padding: 5px 10px;
+  width: 375px;
+  height: 65px;
+
+  .main {
+    background-size: 100% 100%;
+    border-radius: 6px;
+    overflow: hidden;
+    white-space: nowrap;
+    padding: 13px 10px;
+    img {
+      float: left;
+      display: block;
+      height: 16px;
+      padding-left: 15px;
+      width: 28px;
+      vertical-align: middle;
+    }
+    .share-product-text {
+      display: inline-block;
+      vertical-align: middle;
+      white-space: normal;
+      width: 66%;
+      color: #ff4891;
+      font-size: 15px;
+      padding-left: 6px;
+    }
+    .go-share-btn {
+      font-size: 10px;
+      color: #ff4891;
+      position: relative;
+    }
+  }
+}
+
+</style>
